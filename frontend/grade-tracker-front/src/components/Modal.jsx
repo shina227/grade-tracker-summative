bdaf660c0c570ebfbca5766b8e8ea91713947916
Shina <m.atete@alustudent.com>
2025-11-19 23:07:41 +0200

    add course

--- frontend/grade-tracker-front/src/components/Modal.jsx
+++ frontend/grade-tracker-front/src/components/Modal.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 
 const Modal = ({ children, isOpen, onClose, title }) => {
-  return <div>Modal</div>;
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-screen overflow-y-auto overflow-x-hidden bg-black/30">
+      <div className="relative p-4 w-full max-w-2xl max-h-full">
+        {/* Modal content */}
+        <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-200">
+          {/* Modal header */}
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+            <h3 className="text-lg font-medium text-gray-900">{title}</h3>
+            <button
+              type="button"
+              className="flex items-center justify-center w-8 h-8 text-gray-400 bg-transparent rounded-lg text-sm hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
+              onClick={onClose}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1l12 12M13 1L1 13"
+                />
+              </svg>
+            </button>
+          </div>
+
+          {/* Modal body */}
+          <div className="p-4 md:p-5 space-y-4">{children}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
